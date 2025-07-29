3983a3e42d1f56e7ad4bc069bea6e50288bd1da9
shinas-9-04-2002 <shinasshanavas5@gmail.com>
2025-07-29 23:54:00 +0530

    login

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -16,7 +16,7 @@ export const AppContextProvider = ({children})=>{
     const navigate = useNavigate();
     const [user, setUser] = useState(null)
     const [isSeller, setIsSeller] = useState(false)
-    const [showUserLogin, setShowUserLogin] = useState(false)
+    const [showUserLogin, setShowUserLogin] = useState(true)
     const [products, setProducts] = useState([])
 
     const [cartItems, setCartItems] = useState({})
